514226a0b8beb5304affc78a1be5890140a79595
Antonia <62192270+Phantonia@users.noreply.github.com>
2023-07-03 15:40:51 +0200

    Implemented simple flow graph + append

---
+++ src/Phantonia.Historia.Tests/Compiler/FlowGraphTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Phantonia.Historia.Language.Flow;
+
+namespace Phantonia.Historia.Tests.Compiler;
+
+[TestClass]
+public sealed class FlowGraphTests
+{
+    [TestMethod]
+    public void TestSimpleFlowGraph()
+    {
+        FlowGraph simpleGraph = FlowGraph.CreateSimpleFlowGraph(42);
+
+        Assert.AreEqual(42, simpleGraph.StartVertex);
+        Assert.AreEqual(1, simpleGraph.OutgoingEdges.Count);
+        Assert.IsTrue(simpleGraph.OutgoingEdges.ContainsKey(42));
+        Assert.AreEqual(1, simpleGraph.OutgoingEdges[42].Count);
+        Assert.AreEqual(FlowGraph.EmptyNode, simpleGraph.OutgoingEdges[42][0]);
+    }
+
+    [TestMethod]
+    public void TestSimpleAppend()
+    {
+        FlowGraph fgA = FlowGraph.CreateSimpleFlowGraph(42);
+        FlowGraph fgB = FlowGraph.CreateSimpleFlowGraph(64);
+
+        FlowGraph resultGraph = fgA.Append(fgB);
+
+        Assert.AreEqual(42, resultGraph.StartVertex);
+        Assert.AreEqual(2, resultGraph.OutgoingEdges.Count);
+
+        Assert.AreEqual(1, resultGraph.OutgoingEdges[42].Count);
+        Assert.AreEqual(64, resultGraph.OutgoingEdges[42][0]);
+
+        Assert.AreEqual(1, resultGraph.OutgoingEdges[64].Count);
+        Assert.AreEqual(FlowGraph.EmptyNode, resultGraph.OutgoingEdges[64][0]);
+    }
+}
---
+++ src/Phantonia.Historia/Flow/FlowGraph.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using Edges = System.Collections.Immutable.ImmutableDictionary<
+    int, System.Collections.Immutable.ImmutableList<int>>;
+using MutEdges = System.Collections.Generic.Dictionary<
+    int, System.Collections.Generic.List<int>>;
+
+namespace Phantonia.Historia.Language.Flow;
+
+public sealed record FlowGraph
+{
+    public const int EmptyNode = -1;
+
+    public static FlowGraph Empty { get; } = new();
+
+    public static FlowGraph CreateSimpleFlowGraph(int vertex) => Empty with
+    {
+        StartVertex = vertex,
+        OutgoingEdges = Empty.OutgoingEdges.Add(vertex, ImmutableList.Create(EmptyNode)),
+    };
+
+    private FlowGraph() { }
+
+    public Edges OutgoingEdges { get; init; } = Edges.Empty;
+
+    public int StartVertex { get; init; } = EmptyNode;
+
+    public FlowGraph Append(FlowGraph graph)
+    {
+        // this method generates a huge amount of waste - can we optimize this?
+
+        MutEdges tempEdges = OutgoingEdges.ToDictionary(k => k.Key, l => l.Value.ToList());
+
+        foreach ((int key, List<int> value) in tempEdges)
+        {
+            for (int i = 0; i < value.Count; i++)
+            {
+                if (value[i] == EmptyNode)
+                {
+                    value[i] = graph.StartVertex;
+                }
+            }
+        }
+
+        foreach ((int key, ImmutableList<int> value) in graph.OutgoingEdges)
+        {
+            if (tempEdges.ContainsKey(key))
+            {
+                throw new InvalidOperationException("Duplicated vertex key");
+            }
+
+            tempEdges[key] = value.ToList();
+        }
+
+        return this with
+        {
+            OutgoingEdges = tempEdges.ToImmutableDictionary(p => p.Key, p => p.Value.ToImmutableList()),
+        };
+    }
+}
