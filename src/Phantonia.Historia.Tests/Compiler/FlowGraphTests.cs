b8f76db2593668592639666375f1a47e4355befd
Antonia <62192270+Phantonia@users.noreply.github.com>
2023-07-05 16:38:42 +0200

    Implemented Replace and AddVertex to FlowGraph

--- src/Phantonia.Historia.Tests/Compiler/FlowGraphTests.cs
+++ src/Phantonia.Historia.Tests/Compiler/FlowGraphTests.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Phantonia.Historia.Language.Flow;
+using System.Collections.Immutable;
+using System.Linq;
+using Edges = System.Collections.Immutable.ImmutableDictionary<
+    int, System.Collections.Immutable.ImmutableList<int>>;
+using MutEdges = System.Collections.Generic.Dictionary<
+    int, System.Collections.Generic.List<int>>;
 
 namespace Phantonia.Historia.Tests.Compiler;
 
@@ -15,7 +21,7 @@ public sealed class FlowGraphTests
         Assert.AreEqual(1, simpleGraph.OutgoingEdges.Count);
         Assert.IsTrue(simpleGraph.OutgoingEdges.ContainsKey(42));
         Assert.AreEqual(1, simpleGraph.OutgoingEdges[42].Count);
-        Assert.AreEqual(FlowGraph.EmptyNode, simpleGraph.OutgoingEdges[42][0]);
+        Assert.AreEqual(FlowGraph.EmptyVertex, simpleGraph.OutgoingEdges[42][0]);
     }
 
     [TestMethod]
@@ -33,7 +39,7 @@ public sealed class FlowGraphTests
         Assert.AreEqual(64, resultGraph.OutgoingEdges[42][0]);
 
         Assert.AreEqual(1, resultGraph.OutgoingEdges[64].Count);
-        Assert.AreEqual(FlowGraph.EmptyNode, resultGraph.OutgoingEdges[64][0]);
+        Assert.AreEqual(FlowGraph.EmptyVertex, resultGraph.OutgoingEdges[64][0]);
     }
 
     [TestMethod]
@@ -50,5 +56,76 @@ public sealed class FlowGraphTests
         {
             Assert.AreEqual(i, graph.Vertices[i].Index);
         }
+    }
+
+    [TestMethod]
+    public void TestMoreComplexGraph()
+    {
+        FlowGraph graph = FlowGraph.Empty.AddVertex(new FlowVertex { Index = 5 }, 6, 7)
+                                         .AddVertex(new FlowVertex { Index = 6 }, 7, FlowGraph.EmptyVertex)
+                                         .AddVertex(new FlowVertex { Index = 7 }, 8)
+                                         .AddVertex(new FlowVertex { Index = 8 }, 6, FlowGraph.EmptyVertex);
+
+        Assert.AreEqual(5, graph.StartVertex);
+
+        Assert.AreEqual(4, graph.Vertices.Count);
+        Assert.AreEqual(4, graph.OutgoingEdges.Count);
+
+        foreach ((int leftKey, int rightKey) in graph.OutgoingEdges.Keys.OrderBy(k => k).Zip(graph.Vertices.Keys.OrderBy(k => k)))
+        {
+            Assert.AreEqual(leftKey, rightKey);
+            Assert.AreEqual(rightKey, graph.Vertices[rightKey].Index);
+        }
+
+        Assert.AreEqual(2, graph.OutgoingEdges[5].Count);
+        Assert.AreEqual(2, graph.OutgoingEdges[6].Count);
+        Assert.AreEqual(1, graph.OutgoingEdges[7].Count);
+        Assert.AreEqual(2, graph.OutgoingEdges[8].Count);
+
+        Assert.AreEqual(6, graph.OutgoingEdges[5][0]);
+        Assert.AreEqual(7, graph.OutgoingEdges[5][1]);
+        Assert.AreEqual(7, graph.OutgoingEdges[6][0]);
+        Assert.AreEqual(FlowGraph.EmptyVertex, graph.OutgoingEdges[6][1]);
+        Assert.AreEqual(8, graph.OutgoingEdges[7][0]);
+        Assert.AreEqual(6, graph.OutgoingEdges[8][0]);
+        Assert.AreEqual(FlowGraph.EmptyVertex, graph.OutgoingEdges[8][1]);
+    }
+
+    [TestMethod]
+    public void TestReplace()
+    {
+        FlowGraph graphA = FlowGraph.Empty.AddVertex(new FlowVertex { Index = 5 }, 6, 7)
+                                          .AddVertex(new FlowVertex { Index = 6 }, 7, FlowGraph.EmptyVertex)
+                                          .AddVertex(new FlowVertex { Index = 7 }, 8)
+                                          .AddVertex(new FlowVertex { Index = 8 }, 6, FlowGraph.EmptyVertex);
+
+        FlowGraph graphB = FlowGraph.Empty.AddVertex(new FlowVertex { Index = 9 }, 10, 11)
+                                          .AddVertex(new FlowVertex { Index = 10 }, FlowGraph.EmptyVertex)
+                                          .AddVertex(new FlowVertex { Index = 11 }, FlowGraph.EmptyVertex);
+
+        FlowGraph graphC = graphA.Replace(5, graphB);
+
+        Assert.AreEqual(6, graphC.Vertices.Count);
+        Assert.AreEqual(6, graphC.OutgoingEdges.Count);
+
+        foreach ((int leftKey, int rightKey) in graphC.OutgoingEdges.Keys.OrderBy(k => k).Zip(graphC.Vertices.Keys.OrderBy(k => k)))
+        {
+            Assert.AreEqual(leftKey, rightKey);
+            Assert.AreEqual(rightKey, graphC.Vertices[rightKey].Index);
+        }
+
+        Assert.AreEqual(9, graphC.StartVertex);
+
+        void AssertHasEdges(int vertex, params int[] edges)
+        {
+            Assert.IsTrue(edges.SequenceEqual(graphC.OutgoingEdges[vertex].OrderBy(v => v)));
+        }
+
+        AssertHasEdges(9, 10, 11);
+        AssertHasEdges(10, 6, 7);
+        AssertHasEdges(11, 6, 7);
+        AssertHasEdges(6, FlowGraph.EmptyVertex, 7);
+        AssertHasEdges(7, 8);
+        AssertHasEdges(8, FlowGraph.EmptyVertex, 6);
     }
 }
--- src/Phantonia.Historia/Flow/FlowGraph.cs
+++ src/Phantonia.Historia/Flow/FlowGraph.cs
@@ -11,14 +11,14 @@ namespace Phantonia.Historia.Language.Flow;
 
 public sealed record FlowGraph
 {
-    public const int EmptyNode = -1;
+    public const int EmptyVertex = -1;
 
     public static FlowGraph Empty { get; } = new();
 
     public static FlowGraph CreateSimpleFlowGraph(FlowVertex vertex) => Empty with
     {
         StartVertex = vertex.Index,
-        OutgoingEdges = Empty.OutgoingEdges.Add(vertex.Index, ImmutableList.Create(EmptyNode)),
+        OutgoingEdges = Empty.OutgoingEdges.Add(vertex.Index, ImmutableList.Create(EmptyVertex)),
         Vertices = Empty.Vertices.Add(vertex.Index, vertex),
     };
 
@@ -26,9 +26,24 @@ public sealed record FlowGraph
 
     public Edges OutgoingEdges { get; init; } = Edges.Empty;
 
-    public int StartVertex { get; init; } = EmptyNode;
+    public int StartVertex { get; init; } = EmptyVertex;
 
     public ImmutableDictionary<int, FlowVertex> Vertices { get; init; } = ImmutableDictionary<int, FlowVertex>.Empty;
+
+    public FlowGraph AddVertex(FlowVertex vertex, params int[] pointedVertices)
+    {
+        if (Vertices.ContainsKey(vertex.Index))
+        {
+            throw new ArgumentException("Cannot add a vertex that is already there");
+        }
+
+        return this with
+        {
+            Vertices = Vertices.Add(vertex.Index, vertex),
+            OutgoingEdges = OutgoingEdges.Add(vertex.Index, pointedVertices.ToImmutableList()),
+            StartVertex = StartVertex == EmptyVertex ? vertex.Index : StartVertex,
+        };
+    }
 
     public FlowGraph Append(FlowGraph graph)
     {
@@ -37,13 +52,13 @@ public sealed record FlowGraph
         MutEdges tempEdges = OutgoingEdges.ToDictionary(p => p.Key, p => p.Value.ToList());
         Dictionary<int, FlowVertex> tempVertices = Vertices.ToDictionary(p => p.Key, p => p.Value);
 
-        foreach ((int key, List<int> value) in tempEdges)
+        foreach ((int currentVertex, List<int> pointedVertices) in tempEdges)
         {
-            for (int i = 0; i < value.Count; i++)
+            for (int i = 0; i < pointedVertices.Count; i++)
             {
-                if (value[i] == EmptyNode)
+                if (pointedVertices[i] == EmptyVertex)
                 {
-                    value[i] = graph.StartVertex;
+                    pointedVertices[i] = graph.StartVertex;
                 }
             }
         }
@@ -63,6 +78,72 @@ public sealed record FlowGraph
         {
             OutgoingEdges = tempEdges.ToImmutableDictionary(p => p.Key, p => p.Value.ToImmutableList()),
             Vertices = tempVertices.ToImmutableDictionary(),
+        };
+    }
+
+    public FlowGraph Replace(int replacedVertex, FlowGraph graph)
+    {
+        // this method generates a huge amount of waste - can we optimize this?
+
+        if (!Vertices.ContainsKey(replacedVertex))
+        {
+            throw new ArgumentException($"{nameof(replacedVertex)} is not a vertex of this graph.");
+        }
+
+        MutEdges tempEdges = OutgoingEdges.ToDictionary(p => p.Key, p => p.Value.ToList());
+        Dictionary<int, FlowVertex> tempVertices = Vertices.ToDictionary(p => p.Key, p => p.Value);
+
+        // add everything from 'graph' to our graph
+        foreach ((int currentVertex, ImmutableList<int> pointedVertices) in graph.OutgoingEdges)
+        {
+            if (Vertices.ContainsKey(currentVertex))
+            {
+                throw new InvalidOperationException("Duplicated vertex key");
+            }
+
+            tempEdges[currentVertex] = pointedVertices.ToList();
+            tempVertices[currentVertex] = graph.Vertices[currentVertex];
+        }
+
+        // redirect all edges to 'vertex' to 'graph.StartVertex'
+        foreach ((int currentVertex, List<int> pointedVertices) in tempEdges)
+        {
+            for (int i = 0; i < pointedVertices.Count; i++)
+            {
+                if (pointedVertices[i] == replacedVertex)
+                {
+                    pointedVertices[i] = graph.StartVertex;
+                }
+            }
+        }
+
+        // redirect all edges to 'graph's EmptyVertex to every vertex that 'replacedVertex' points to
+        ImmutableList<int> replacedVertexPointedVertices = OutgoingEdges[replacedVertex];
+
+        foreach ((int currentVertex, ImmutableList<int> pointedVertices) in graph.OutgoingEdges)
+        {
+            for (int i = 0; i < pointedVertices.Count; i++)
+            {
+                if (pointedVertices[i] == EmptyVertex)
+                {
+                    tempEdges[currentVertex].RemoveAt(i);
+
+                    for (int j = 0; j < replacedVertexPointedVertices.Count; j++)
+                    {
+                        tempEdges[currentVertex].Add(replacedVertexPointedVertices[j]);
+                    }
+                }
+            }
+        }
+
+        tempVertices.Remove(replacedVertex);
+        tempEdges.Remove(replacedVertex);
+
+        return this with
+        {
+            OutgoingEdges = tempEdges.ToImmutableDictionary(p => p.Key, p => p.Value.ToImmutableList()),
+            Vertices = tempVertices.ToImmutableDictionary(),
+            StartVertex = StartVertex == replacedVertex ? graph.StartVertex : StartVertex,
         };
     }
 }
