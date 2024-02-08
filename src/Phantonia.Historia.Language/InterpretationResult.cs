1fbeecc8ae1c24e2c778cd1a36c7674a1c012cf9
Antonia <62192270+Phantonia@users.noreply.github.com>
2024-02-08 17:43:25 +0100

    (untested) developed interpreter

---
+++ src/Phantonia.Historia.Language/InterpretationResult.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Phantonia.Historia.Language;
+
+public readonly record struct InterpretationResult
+{
+    public InterpretationResult() { }
+
+    public ImmutableArray<Error> Errors { get; init; } = ImmutableArray<Error>.Empty;
+
+    public InterpreterStateMachine? StateMachine { get; init; }
+
+    [MemberNotNull(nameof(StateMachine))]
+    public bool IsValid
+    {
+        get
+        {
+            Debug.Assert(Errors.Length == 0 ^ StateMachine is null); // xor
+            return StateMachine is not null;
+        }
+    }
+}
---
+++ src/Phantonia.Historia.Language/RecordInstance.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Immutable;
+
+namespace Phantonia.Historia.Language;
+
+public sealed record RecordInstance
+{
+    public required string RecordName { get; init; }
+
+    public required ImmutableDictionary<string, object> Properties { get; init; }
+}
