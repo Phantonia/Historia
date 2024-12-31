74240413ee6d9eb9be7b165da07534d8fa93aa0d
Antonia <62192270+Phantonia@users.noreply.github.com>
2024-12-31 13:15:37 +0100

    Added support to compile multiple hstr files into one story

---
+++ src/Phantonia.Historia.Build/ConcatenatedStream.cs
@@ -0,0 +1,76 @@
+﻿namespace Phantonia.Historia.Build;
+
+// adapted from https://stackoverflow.com/questions/3879152/how-do-i-concatenate-two-system-io-stream-instances-into-one
+internal sealed class ConcatenatedStream(IEnumerable<Stream> streams) : Stream
+{
+    private readonly Queue<Stream> streams = new(streams);
+
+    public override bool CanRead => true;
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        int totalBytesRead = 0;
+
+        while (count > 0 && streams.Count > 0)
+        {
+            int bytesRead = streams.Peek().Read(buffer, offset, count);
+            if (bytesRead == 0)
+            {
+                streams.Dequeue().Dispose();
+                continue;
+            }
+
+            totalBytesRead += bytesRead;
+            offset += bytesRead;
+            count -= bytesRead;
+        }
+
+        return totalBytesRead;
+    }
+
+    public override bool CanSeek => false;
+
+    public override bool CanWrite => false;
+
+    public override void Flush()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override long Length => throw new NotImplementedException();
+
+    public override long Position
+    {
+        get
+        {
+            throw new NotImplementedException();
+        }
+        set
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void SetLength(long value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        foreach (Stream stream in streams)
+        {
+            stream.Dispose();
+        }
+    }
+}
--- src/Phantonia.Historia.Build/Program.cs
+++ src/Phantonia.Historia.Build/Program.cs
@@ -5,12 +5,12 @@ namespace Phantonia.Historia.Build;
 
 internal static class Program
 {
+    private const string InputPath = @"C:\Users\User\Documents\Projects\Stories\AntiFairytale\Historia";
+    private const string OutputPath = @"C:\Users\User\Documents\Godot\AntiFairytaleTextAdventure\Historia.cs";
+    
     private static void Main(string[] args)
     {
-        const string InputPath = @"C:\Users\User\Documents\Projects\Stories\AntiFairytale\Historia\Ch1_FreedomBreaksFree.hstr";
-        const string OutputPath = @"C:\Users\User\Documents\Godot\AntiFairytaleTextAdventure\Historia.cs";
-
-        using TextReader inputReader = new StreamReader(InputPath);
+        using TextReader inputReader = new StreamReader(GetInputStream());
         using TextWriter outputWriter = new StreamWriter(OutputPath);
 
         Stopwatch sw = Stopwatch.StartNew();
@@ -26,12 +26,26 @@ internal static class Program
 
         Console.WriteLine($"Compilation did not succeed after {sw.Elapsed.TotalSeconds} seconds.");
 
-        string code = File.ReadAllText(InputPath);
+        string code = new StreamReader(GetInputStream()).ReadToEnd();
 
         foreach (Error error in result.Errors)
         {
             Console.WriteLine(Errors.GenerateFullMessage(code, error));
             Console.WriteLine();
         }
+    }
+
+    private static Stream GetInputStream()
+    {
+        IEnumerable<string> files = Directory.GetFiles(InputPath, "*.hstr");
+
+        List<Stream> streams = [];
+
+        foreach (string hstrFile in files)
+        {
+            streams.Add(new FileStream(hstrFile, FileMode.Open, FileAccess.Read));
+        }
+
+        return new ConcatenatedStream(streams);
     }
 }
