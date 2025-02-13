08cb222c2cd72fb7c7131d71001d9430400bf4dc
Antonia <62192270+Phantonia@users.noreply.github.com>
2025-02-13 17:23:37 +0100

    Added RefactorFiles method

--- src/Phantonia.Historia.Language/Refactory.cs
+++ src/Phantonia.Historia.Language/Refactory.cs
@@ -1,4 +1,5 @@
 ﻿using Phantonia.Historia.Language.LexicalAnalysis;
+using Phantonia.Historia.Language.SemanticAnalysis;
 using Phantonia.Historia.Language.SyntaxAnalysis;
 using System.Collections.Generic;
 using System.Collections.Immutable;
@@ -40,5 +41,69 @@ public static class Refactory
         story.CompilationUnits[0].Reconstruct(writer);
 
         return writer.ToString();
+    }
+
+    public static bool RefactorFiles(string directory, IEnumerable<string> inputPaths, params IRefactoring[] refactorings)
+    {
+        List<Error> errors = [];
+
+        List<CompilationUnitNode> compilationUnits = [];
+        long previousLength = 0;
+
+        Dictionary<string, ImmutableArray<long>> pathLines = [];
+
+        foreach (string path in inputPaths)
+        {
+            string absolutePath = Path.Combine(directory, path);
+            using StreamReader inputReader = new(absolutePath);
+
+            Lexer lexer = new(inputReader, indexOffset: previousLength);
+            lexer.ErrorFound += errors.Add;
+            ImmutableArray<Token> tokens = lexer.Lex(out IEnumerable<long> lineIndices);
+            lexer.ErrorFound -= errors.Add;
+
+            pathLines[path] = [.. lineIndices];
+
+            Parser parser = new(tokens, path);
+            parser.ErrorFound += errors.Add;
+            CompilationUnitNode unit = parser.Parse();
+            parser.ErrorFound -= errors.Add;
+
+            compilationUnits.Add(unit);
+            previousLength += unit.Length;
+        }
+
+        StoryNode story = new()
+        {
+            CompilationUnits = [.. compilationUnits],
+            Index = 0,
+            Length = previousLength,
+            PrecedingTokens = [],
+        };
+
+        Binder binder = new(story);
+        binder.ErrorFound += errors.Add;
+        BindingResult bindingResult = binder.Bind();
+        binder.ErrorFound -= errors.Add;
+
+        if (!bindingResult.IsValid || errors.Count > 0)
+        {
+            return false;
+        }
+
+        story = bindingResult.BoundStory;
+
+        foreach (IRefactoring refactoring in refactorings)
+        {
+            story = refactoring.Refactor(story);
+        }
+
+        foreach (CompilationUnitNode unit in story.CompilationUnits)
+        {
+            using StreamWriter writer = new(Path.Combine(directory, unit.Path));
+            unit.Reconstruct(writer);
+        }
+
+        return true;
     }
 }
