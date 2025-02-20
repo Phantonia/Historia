a429d9fbc2003ef9ff4ce1f580a338378af3f830
Antonia <62192270+Phantonia@users.noreply.github.com>
2025-02-20 14:45:29 +0100

    Added test for former line refactoring bug

--- src/Phantonia.Historia.Tests/Refactorings/LineRefactoringTests.cs
+++ src/Phantonia.Historia.Tests/Refactorings/LineRefactoringTests.cs
@@ -12,99 +12,103 @@ public sealed class LineRefactoringTests
     {
         string original =
             """"
-            enum Character(Alice, Bob);
-            line record Line(Character: Character, Text: String);
-            line record TaggedLine(Character: String, Tag: Int, Text: String);
-            union Output(Line, TaggedLine, Int);
+        enum Character(Alice, Bob);
+        line record Line(Character: Character, Text: String);
+        line record TaggedLine(Character: String, Tag: Int, Text: String);
+        union Output(Line, TaggedLine, Int);
 
-            setting OutputType: Output;
-            setting OptionType: String;
+        setting OutputType: Output;
+        setting OptionType: String;
 
-            chapter main
+        chapter main
+        {
+            output Line(Character.Alice, "Hello world");
+            output TaggedLine("Charlie", 62, "Goodbye world");
+            
+            switch 0
             {
-                output Line(Character.Alice, "Hello world");
-                output TaggedLine("Charlie", 62, "Goodbye world");
-                
-                switch 0
+                option "abc"
                 {
-                    option "abc"
-                    {
-                        output Line(Character.Bob, '"Blablabla"');
-                    }
-
-                    option "xyz"
-                    {
-                        output TaggedLine("Danielle", 5, 'uwu');
-                    }
+                    output Line(Character.Bob, '"Blablabla"');
                 }
 
-                loop switch 1
+                option "xyz"
                 {
-                    option "def"
-                    {
-                        output Line(Character.Alice, "eeee");
-                    }
-
-                    loop option "ghi"
-                    {
-                        output Line(Character.Bob, """o"uouo"u""");
-                    }
-
-                    final option "jkl"
-                    {
-                        output TaggedLine("Charlie", 91, "");
-                    }
+                    output TaggedLine("Danielle", 5, 'uwu');
                 }
             }
-            """";
+
+            loop switch 1
+            {
+                option "def"
+                {
+                    output Line(Character.Alice, "eeee");
+                }
+
+                loop option "ghi"
+                {
+                    output Line(Character.Bob, """o"uouo"u""");
+                }
+
+                final option "jkl"
+                {
+                    output TaggedLine("Charlie", 91, "");
+                }
+            }
+
+            Alice: "z";
+        }
+        """";
 
         string refactored =
             """"
-            enum Character(Alice, Bob);
-            line record Line(Character: Character, Text: String);
-            line record TaggedLine(Character: String, Tag: Int, Text: String);
-            union Output(Line, TaggedLine, Int);
+        enum Character(Alice, Bob);
+        line record Line(Character: Character, Text: String);
+        line record TaggedLine(Character: String, Tag: Int, Text: String);
+        union Output(Line, TaggedLine, Int);
+        
+        setting OutputType: Output;
+        setting OptionType: String;
+        
+        chapter main
+        {
+            Alice: "Hello world";
+            "Charlie" [62]: "Goodbye world";
             
-            setting OutputType: Output;
-            setting OptionType: String;
-            
-            chapter main
+            switch 0
             {
-                Alice: "Hello world";
-                "Charlie" [62]: "Goodbye world";
-                
-                switch 0
+                option "abc"
                 {
-                    option "abc"
-                    {
-                        Bob: '"Blablabla"';
-                    }
-            
-                    option "xyz"
-                    {
-                        "Danielle" [5]: 'uwu';
-                    }
+                    Bob: '"Blablabla"';
                 }
-
-                loop switch 1
+        
+                option "xyz"
                 {
-                    option "def"
-                    {
-                        Alice: "eeee";
-                    }
-
-                    loop option "ghi"
-                    {
-                        Bob: """o"uouo"u""";
-                    }
-
-                    final option "jkl"
-                    {
-                        "Charlie" [91]: "";
-                    }
+                    "Danielle" [5]: 'uwu';
                 }
             }
-            """";
+
+            loop switch 1
+            {
+                option "def"
+                {
+                    Alice: "eeee";
+                }
+
+                loop option "ghi"
+                {
+                    Bob: """o"uouo"u""";
+                }
+
+                final option "jkl"
+                {
+                    "Charlie" [91]: "";
+                }
+            }
+
+            Alice: "z";
+        }
+        """";
 
         string result = Refactory.RefactorString(original, new LineRefactoring());
         Assert.AreEqual(refactored, result);
